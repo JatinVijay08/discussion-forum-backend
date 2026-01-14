9f93b28336e93a1f5bff72200b19779c5b22b79a
JatinVijay08 <jatinvijayvargiya0805@gmail.com>
2026-01-14 12:46:45 +0530

    Initital Controller Health check done

---
+++ src/main/java/com/jatin/forum/Demo.java
@@ -0,0 +1,12 @@
+package com.jatin.forum;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Demo {
+    @GetMapping("/api/health")
+    public String health() {
+        return "OK";
+    }
+}
---
+++ src/main/java/com/jatin/forum/DiscussionForumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jatin.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscussionForumBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscussionForumBackendApplication.class, args);
+    }
+
+}
