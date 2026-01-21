443d25f0cddae67b66ca099a5acf53116f73ff71
JatinVijay08 <jatinvijayvargiya0805@gmail.com>
2026-01-21 21:10:32 +0530

    Fixed N+1 Query problem using JOIN FETCH explicit query on comments and user fetching

--- src/main/java/com/jatin/forum/repository/CommentRepo.java
+++ src/main/java/com/jatin/forum/repository/CommentRepo.java
@@ -4,8 +4,14 @@ import com.jatin.forum.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface CommentRepo extends JpaRepository<Comment, Long> {
+    @Query("""
+          SELECT c from Comment c
+          Join fetch c.user
+          where c.post.id = :postId
+""")
     Page<Comment> findByPostId(Long postId, Pageable pageable);
 }
